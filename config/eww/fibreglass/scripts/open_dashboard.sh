c37ec59acdef77d90f8216289d8cf073398ce5e1
あらゆる猫のディーラー <xhackerqwertyx@protonmail.com>
2025-03-29 10:49:24 +1300

    MAJOR UPDATE WITH EWW

---
+++ config/eww/fibreglass/scripts/open_dashboard.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "$(~/eww-source/target/release/eww active-windows)" == *"dashboard"* || "$(/home/catdealer/eww-source/target/release/eww active-windows)" == "" ]]; then
+
+  if [[ "$(~/eww-source/target/release/eww active-windows)" == *"dashboard"* ]]; then
+    ~/eww-source/target/release/eww close dashboard
+  else
+    killall eww
+
+    eww_window_id=0
+
+    for eww_monitor in $(xrandr -q | grep " connected" | cut -d ' ' -f1); do
+      ~/eww-source/target/release/eww -c ~/.config/eww/ open --toggle bar --id "$eww_window_id" --screen $eww_monitor &
+      disown
+      eww_window_id+=1
+    done
+    if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
+      eww open currentnotificationsbox --screen ${EXTERNAL_MONITOR} & disown
+	else
+      eww open currentnotificationsbox --screen ${INTERNAL_MONITOR} & disown
+    if
+  fi
+  eww update reveal_db=false
+else
+  if [[ $(xrandr -q | grep "HDMI-1 connected") ]]; then
+    ~/eww-source/target/release/eww open dashboard --screen HDMI-1
+  else
+    ~/eww-source/target/release/eww open dashboard --screen eDP-1
+  fi
+  
+  sleep 0.3
+  eww update reveal_db=true
+fi
