15e35034ee129212f5a9a1debdc409ec51dc457c
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-16 18:20:45 +1200

    matugen

--- .config/scripts/setBordersSway.sh
+++ .config/scripts/setBordersSway.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-swaymsg client.focused "#151311" "#151311" "#e8e1dd" "#151311"
-swaymsg client.unfocused "#221f1d" "#221f1d" "#e8e1dd" "#221f1d"
-swaymsg client.focused_inactive "#221f1d" "#221f1d" "#e8e1dd" "#221f1d"
-swaymsg client.urgent "#151311" "#151311" "#e8e1dd" "#151311"
+swaymsg client.focused "#131412" "#131412" "#e4e2df" "#131412"
+swaymsg client.unfocused "#1f201e" "#1f201e" "#e4e2df" "#1f201e"
+swaymsg client.focused_inactive "#1f201e" "#1f201e" "#e4e2df" "#1f201e"
+swaymsg client.urgent "#131412" "#131412" "#e4e2df" "#131412"
